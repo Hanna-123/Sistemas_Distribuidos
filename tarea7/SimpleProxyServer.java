3f219d76d907f04ada95550edc665a277a9898d8
Hanna <shadowemo13@hotmail.com>
2021-01-11 17:04:39 -0600

    proxy ok

--- tarea7/SimpleProxyServer.java
+++ tarea7/SimpleProxyServer.java
@@ -1,166 +1,166 @@
-// Este ejemplo es de _Java Examples in a Nutshell_. (http://www.oreilly.com)
-// Copyright (c) 1997 por David Flanagan
-// Este ejemplo se proporciona SIN NINGUNA GARANTÍA, ya sea expresa o implícita.
-// Puede estudiarlo, utilizarlo, modificarlo y distribuirlo con fines no comerciales.
-// Para cualquier uso comercial, consulte http://www.davidflanagan.com/javaexamples
+// This example is from _Java Examples in a Nutshell_. (http://www.oreilly.com)
+// Copyright (c) 1997 by David Flanagan
+// This example is provided WITHOUT ANY WARRANTY either expressed or implied.
+// You may study, use, modify, and distribute it for non-commercial purposes.
+// For any commercial use, see http://www.davidflanagan.com/javaexamples
 
-/ * CPG * / // Fuente: https://resources.oreilly.com/examples/9781565923713/blob/master/SimpleProxyServer.java
-/ * CPG * / // Modificado por Carlos Pineda G. 2020
+/*CPG*/ // Fuente: https://resources.oreilly.com/examples/9781565923713/blob/master/SimpleProxyServer.java
+/*CPG*/ // Modificado por Carlos Pineda G. 2020
 
-importar java.io. *;
-importar java.net. *;
+import java.io.*;
+import java.net.*;
 
-/ **
- * Esta clase implementa un servidor proxy simple de un solo subproceso.
- ** /
+/**
+ * This class implements a simple single-threaded proxy server.
+ **/
 public class SimpleProxyServer {
-  / ** El método principal analiza los argumentos y los pasa a runServer * /
-  public static void main (String [] args) lanza IOException {
-    tratar {
-      // Verifica la cantidad de argumentos
-      / * CPG * / // argumentos: host-remoto puerto-remoto puerto-local puerto-server2
-      / * CPG * / if (args.length! = 3) 
-      si (args.length! = 4) 
-        lanzar nueva IllegalArgumentException ("Número incorrecto de argumentos");
+  /** The main method parses arguments and passes them to runServer */
+  public static void main(String[] args) throws IOException {
+    try {
+      // Check the number of arguments
+      /*CPG*/ // argumentos: host-remoto puerto-remoto puerto-local puerto-server2
+      /*CPG*/ if (args.length != 3) 
+      if (args.length != 4) 
+        throw new IllegalArgumentException("Wrong number of arguments.");
 
-      // Obtener los argumentos de la línea de comandos: el host y el puerto para los que somos proxy
-      // y el puerto local en el que escuchamos las conexiones
-      Cadena host = args [0];
-      int puerto remoto = Integer.parseInt (args [1]);
-      int localport = Integer.parseInt (args [2]);
-      / * CPG * / int localport2 = Integer.parseInt (args [3]); // puerto del server2
-      // Imprime un mensaje de inicio
-      System.out.println ("Proxy de inicio para" + host + ":" + puerto remoto +
-                         "en el puerto" + puerto local);
-      // Y empieza a ejecutar el servidor
-      / * CPG * / // runServer (host, puerto remoto, puerto local); // nunca regresa
-      runServer (host, puerto remoto, puerto local, puerto local2); // nunca regresa
+      // Get the command-line arguments: the host and port we are proxy for
+      // and the local port that we listen for connections on
+      String host = args[0];
+      int remoteport = Integer.parseInt(args[1]);
+      int localport = Integer.parseInt(args[2]);
+      /*CPG*/ int localport2 = Integer.parseInt(args[3]); // puerto del server2
+      // Print a start-up message
+      System.out.println("Starting proxy for " + host + ":" + remoteport +
+                         " on port " + localport);
+      // And start running the server
+      /*CPG*/ // runServer(host, remoteport, localport);   // never returns
+      runServer(host, remoteport, localport,localport2);   // never returns
     } 
-    captura (Excepción e) {
-      System.err.println (e);
-      System.err.println ("Uso: java SimpleProxyServer" +
-                         / * CPG * / // "<host> <remoteport> <localport>");
-                         "<host-remoto> <puertoremoto> <puertolocal> <puerto-servidor2>");
+    catch (Exception e) {
+      System.err.println(e);
+      System.err.println("Usage: java SimpleProxyServer " +
+                         /*CPG*/ // "<host> <remoteport> <localport>");
+                         "<remote-host> <remoteport> <localport> <server2-port>");
     }
   }
 
-  / **
-   * Este método ejecuta un servidor proxy de un solo subproceso para 
-   * host: puerto remoto en el puerto local especificado. Nunca regresa.
-   ** /
-  / * CPG * / // public static void runServer (String host, int remoteport, int localport) 
-  public static void runServer (String host, int remoteport, int localport, int localport2) 
-       lanza IOException {
-    // Crea un ServerSocket para escuchar conexiones con
-    ServerSocket ss = nuevo ServerSocket (puerto local);
+  /**
+   * This method runs a single-threaded proxy server for 
+   * host:remoteport on the specified local port.  It never returns.
+   **/
+  /*CPG*/ // public static void runServer(String host, int remoteport, int localport) 
+  public static void runServer(String host, int remoteport, int localport, int localport2) 
+       throws IOException {
+    // Create a ServerSocket to listen for connections with
+    ServerSocket ss = new ServerSocket(localport);
 
-    // Cree búferes para la comunicación de cliente a servidor y de servidor a cliente.
-    // Hacemos una final para que se pueda usar en una clase anónima a continuación.
-    // Tenga en cuenta las suposiciones sobre el volumen de tráfico en cada dirección ...
-    byte final [] solicitud = nuevo byte [1024];
-    byte [] respuesta = nuevo byte [4096];
+    // Create buffers for client-to-server and server-to-client communication.
+    // We make one final so it can be used in an anonymous class below.
+    // Note the assumptions about the volume of traffic in each direction...
+    final byte[] request = new byte[1024];
+    byte[] reply = new byte[4096];
     
-    // Este es un servidor que nunca regresa, así que ingrese un ciclo infinito.
-    while (verdadero) { 
-      // Variables para sujetar los sockets al cliente y al servidor.
-      Socket cliente = nulo, servidor = nulo;
-      / * CPG * / Socket server2 = null;
-      tratar {
-        // Espere una conexión en el puerto local
-        cliente = ss.accept ();
+    // This is a server that never returns, so enter an infinite loop.
+    while(true) { 
+      // Variables to hold the sockets to the client and to the server.
+      Socket client = null, server = null;
+      /*CPG*/ Socket server2 = null;
+      try {
+        // Wait for a connection on the local port
+        client = ss.accept();
         
-        // Obtener flujos de clientes. Hazlos definitivos para que puedan
-        // se utilizará en el hilo anónimo a continuación.
-        InputStream final from_client = client.getInputStream ();
-        OutputStream final to_client = client.getOutputStream ();
+        // Get client streams.  Make them final so they can
+        // be used in the anonymous thread below.
+        final InputStream from_client = client.getInputStream();
+        final OutputStream to_client= client.getOutputStream();
 
-        // Establece una conexión con el servidor real
-        // Si no podemos conectarnos al servidor, enviamos un error al 
-        // cliente, desconecte, luego continúe esperando otra conexión.
-        intente {servidor = nuevo Socket (host, puerto remoto); 
-	/ * CPG * / // en este caso el puerto del servidor2 se define como el puerto local mas uno
-	/ * CPG * / server2 = new Socket ("localhost", localport2);
+        // Make a connection to the real server
+        // If we cannot connect to the server, send an error to the 
+        // client, disconnect, then continue waiting for another connection.
+        try { server = new Socket(host, remoteport); 
+	/*CPG*/ // en este caso el puerto del servidor2 se define como el puerto local mas uno
+	/*CPG*/ server2 = new Socket("localhost", localport2);
 	} 
         catch (IOException e) {
-          PrintWriter out = new PrintWriter (nuevo OutputStreamWriter (to_client));
-          out.println ("El servidor proxy no se puede conectar a" + host + ":" +
-                      puerto remoto + ": \ n" + e);
-          out.flush ();
-          cliente.close ();
-          Seguir;
+          PrintWriter out = new PrintWriter(new OutputStreamWriter(to_client));
+          out.println("Proxy server cannot connect to " + host + ":" +
+                      remoteport + ":\n" + e);
+          out.flush();
+          client.close();
+          continue;
         }
 
-        // Obtiene las transmisiones del servidor.
-        InputStream final from_server = server.getInputStream ();
-        OutputStream final to_server = server.getOutputStream ();
-	/ * CPG * / final InputStream from_server2 = server2.getInputStream ();
-	/ * CPG * / OutputStream final a_servidor2 = servidor2.getOutputStream ();
+        // Get server streams.
+        final InputStream from_server = server.getInputStream();
+        final OutputStream to_server = server.getOutputStream();
+	/*CPG*/ final InputStream from_server2 = server2.getInputStream();
+	/*CPG*/ final OutputStream to_server2 = server2.getOutputStream();
 
-        // Cree un hilo para leer las solicitudes del cliente y pasarlas al 
-        // servidor. Tenemos que usar un hilo separado porque las solicitudes y
-        // las respuestas pueden ser asincrónicas.
-        Hilo t = nuevo Hilo () {
-          public void run () {
+        // Make a thread to read the client's requests and pass them to the 
+        // server.  We have to use a separate thread because requests and
+        // responses may be asynchronous.
+        Thread t = new Thread() {
+          public void run() {
             int bytes_read;
-            tratar {
-              while ((bytes_read = from_client.read (solicitud))! = -1) {
-                to_server.write (solicitud, 0, bytes_read);
-                to_server.flush ();
-		/ * CPG * / to_server2.write (solicitud, 0, bytes_read);
-		/ * CPG * / to_server2.flush ();
+            try {
+              while((bytes_read = from_client.read(request)) != -1) {
+                to_server.write(request, 0, bytes_read);
+                to_server.flush();
+		/*CPG*/ to_server2.write(request, 0, bytes_read);
+		/*CPG*/ to_server2.flush();
               }
             }
             catch (IOException e) {}
 
-            // el cliente nos cerró la conexión, así que cierre nuestro 
-            // conexión al servidor. Esto también hará que
-            // bucle de servidor a cliente en la salida del hilo principal.
-            intente {to_server.close ();
-		/ * CPG * / to_server2.close ();
-	    } captura (IOException e) {}
+            // the client closed the connection to us, so  close our 
+            // connection to the server.  This will also cause the 
+            // server-to-client loop in the main thread exit.
+            try {to_server.close();
+		/*CPG*/ to_server2.close();
+	    } catch (IOException e) {}
           }
         };
 
-        // Iniciar la ejecución del hilo de solicitud de cliente a servidor
-        t.start ();  
+        // Start the client-to-server request thread running
+        t.start();  
 
-        // Mientras tanto, en el hilo principal, lee las respuestas del servidor
-        // y devolverlos al cliente. Esto se hará en
-        // en paralelo con el hilo de solicitud de cliente a servidor anterior.
+        // Meanwhile, in the main thread, read the server's responses
+        // and pass them back to the client.  This will be done in
+        // parallel with the client-to-server request thread above.
         int bytes_read;
-	/ * CPG * / 
+	/*CPG*/ 
 	// la respuesta del server (remoto) no se necesita por tanto se ignora
-	/ *	
-        tratar {
-          while ((bytes_read = from_server.read (respuesta))! = -1) {
-            to_client.write (respuesta, 0, bytes_read);
-            to_client.flush ();
+	/*	
+        try {
+          while((bytes_read = from_server.read(reply)) != -1) {
+            to_client.write(reply, 0, bytes_read);
+            to_client.flush();
           }
         }
-        catch (IOException e) {}
- 	* /	
+        catch(IOException e) {}
+ 	*/	
 	// se envia al cliente la respuesta del server2 (local)
-        tratar {
-          while ((bytes_read = from_server2.read (respuesta))! = -1) {
-            to_client.write (respuesta, 0, bytes_read);
-            to_client.flush ();
+        try {
+          while((bytes_read = from_server2.read(reply)) != -1) {
+            to_client.write(reply, 0, bytes_read);
+            to_client.flush();
           }
         }
-        catch (IOException e) {}
+        catch(IOException e) {}
 
-        // El servidor cerró su conexión con nosotros, así que cierre nuestro 
-        // conexión con nuestro cliente. Esto hará que el otro hilo salga.
-        to_client.close ();
+        // The server closed its connection to us, so close our 
+        // connection to our client.  This will make the other thread exit.
+        to_client.close();
       }
-      catch (IOException e) {System.err.println (e); }
-      // Cierre los sockets sin importar lo que pase cada vez que pase por el bucle.
-      finalmente { 
-        tratar { 
-          si (servidor! = nulo) servidor.close (); 
-	  / * CPG * / if (servidor2! = Nulo) servidor.close ();
-          if (cliente! = nulo) cliente.close (); 
+      catch (IOException e) { System.err.println(e); }
+      // Close the sockets no matter what happens each time through the loop.
+      finally { 
+        try { 
+          if (server != null) server.close(); 
+	  /*CPG*/ if (server2 != null) server.close();
+          if (client != null) client.close(); 
         }
-        catch (IOException e) {}
+        catch(IOException e) {}
       }
     }
   }
